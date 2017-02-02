8a58618145fcd97f2435e17e26e76a4a330471ac
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-01 18:03:34 -0800

    Created user page

--- server.py
+++ server.py
@@ -27,6 +27,7 @@ def index():
     """Homepage."""
     # a = jsonify([1,3])
 
+
     return render_template("homepage.html")
 
 
@@ -38,13 +39,13 @@ def user_list():
     return render_template("user_list.html", users=users)
 
 
-@app.route('/register', methods=["GET"])
+@app.route('/login', methods=["GET"])
 def register_form():
 
     return render_template("user_login.html")
 
 
-@app.route('/register', methods=["POST"])
+@app.route('/login', methods=["POST"])
 def register_process():
 
     username = request.form.get("username")
@@ -54,12 +55,13 @@ def register_process():
     try:
         db.session.query(User).filter_by(email=username).one().email
         password == db.session.query(User).filter_by(email=username).one().password
-    except:
+    except NoResultFound:
         flash("Login information inccorect")
-        return redirect("/register")
+        return redirect("/login")
 
     session['username'] = username
     flash("Logged in!")
+
 
     return redirect("/")
 
@@ -77,6 +79,62 @@ def logout_complete():
     flash("Logged out!")
 
     return redirect("/")
+
+
+@app.route('/register', methods=["GET"])
+def register():
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=["POST"])
+def register_complete():
+
+    email = request.form.get("email")
+
+    password = request.form.get("password")
+
+    age = request.form.get("age")
+
+    zipcode = request.form.get("zipcode")
+
+    new_user = User(email=email,
+                    password=password,
+                    age=age,
+                    zipcode=zipcode)
+
+
+    db.session.add(new_user)
+
+    db.session.commit()
+
+    session['username'] = email
+    flash("Logged in!")
+
+    return redirect("/")
+
+@app.route('/users/<user_id>')
+def display_user(user_id):
+
+
+    age = db.session.query(User).filter_by(user_id=user_id).one().age  
+    
+    zipcode = db.session.query(User).filter_by(user_id=user_id).one().zipcode
+
+    movies = db.session.query(Rating).filter_by(user_id=user_id).all().movie_id
+
+    movie_titles = []
+
+    # Start here on Thursday
+
+    for movie_id in movies:
+        movie_name = db.session.query(Movie).filter_by(movie_id=movie_id).all().title
+        movie_titles.append(movie_name)
+
+    return render_template("user_details.html",
+                    age=age,
+                    zipcode=zipcode,
+                    movie_titles=movie_titles)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
